b9d4820b636c759e3c197de7628db75e9145aa86
Archana Bansal <archanamittal0388@gmail.com>
2018-03-12 17:10:24 +0530

    number parser added

--- parser.js
+++ parser.js
@@ -1,10 +1,10 @@
 const booleanParser = input => {
-	if (input.indexOf("true") === 0) {
-		return [true, input.slice(4, input.length)];
+	if (input.startsWith("true")) {
+		return [true, input.slice(4)];
 	}
 
-	if (input.indexOf("false") === 0) {
-		return [false, input.slice(5, input.length)];
+	if (input.startsWith("false")) {
+		return [false, input.slice(5)];
 	}
 
 	return null;
@@ -14,13 +14,27 @@ const booleanParser = input => {
 //console.log(booleanParser("fal123"))// op-- null
 
 const nullParser = input => {
-	if (input.indexOf("null") === 0) {
-		return [null, input.slice(4, input.length)];
+	if (input.startsWith("null")) {
+		return [null, input.slice(4)];
 	}
 	return null;
 };
 
 //console.log(nullParser("null123"))// op--[null, "123"]
 //console.log(nullParser("123null"))// op-- null
-//console.log(nullParser12("null")) // op--[null, ""]
-//console.log(nullParser12("123"))// op-- null
+//console.log(nullParser("null")) // op--[null, ""]
+//console.log(nullParser("123"))// op-- null
+
+const numberParser = input => {
+	let regexNum = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/;
+	let num = input.match(regexNum);
+	if (num !== null) {
+		return [parseFloat(num[0]), input.slice(num[0].length)];
+	}
+	return null;
+};
+
+// console.log(numberParser("12.38888jhgjghj"));
+// console.log(numberParser("-123jhgjghj"));
+// console.log(numberParser("7.71234e+2jhgjghj"));
+// console.log(numberParser("0000jhgjghj"));

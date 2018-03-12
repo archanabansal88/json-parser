491fef30959151e4352c996a6316cd03d73704c0
Archana Bansal <archanamittal0388@gmail.com>
2018-03-12 14:44:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GeekSkool - JSON Parser Project
---
+++ parser.js
@@ -0,0 +1,45 @@
+const objectParse = input => {};
+
+/* 4. // create an empty object
+
+let string = "{"a":"hello","b":"567","c":"asd"}"
+
+let i = 0;
+let obj={};
+if string[i] = '{'
+while(string[i] === '}'){
+2.  //extract key string.slice(i+1, string.indexOf(':'))
+3. // extract value string.slice(string.indexof(':')+1, string.indexof(','))
+ i = i + key.length + value.length+2
+
+4. // add key value pair to the object
+}
+
+
+
+"{"a":"hello","b":[1,2,null,3,4],"c":"asd"}"
+
+i=0;
+key: a
+value: hello
+i = 0+ 1+ 5+ 2 = 8
+
+key: b
+value: [1,2,null,3,4]
+
+
+
+object
+booean
+string
+number
+null
+array
+
+
+// code .. break your functions into smaller function
+
+"{"a":"hello","b":[1,2,null,3,4],"c":"asd"}"
+
+pointer---current position
+ */

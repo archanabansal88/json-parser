d10c2dffb4d82b53ddfea0798240712703c67e58
Archana Bansal <archanamittal0388@gmail.com>
2018-03-13 16:10:52 +0530

    string const arrayParser = input => {

--- parser.js
+++ parser.js
@@ -28,7 +28,7 @@ const nullParser = input => {
 const numberParser = input => {
 	let regexNum = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/;
 	let num = input.match(regexNum);
-	if (num !== null) {
+	if (num) {
 		return [parseFloat(num[0]), input.slice(num[0].length)];
 	}
 	return null;
@@ -38,3 +38,38 @@ const numberParser = input => {
 // console.log(numberParser("-123jhgjghj"));
 // console.log(numberParser("7.71234e+2jhgjghj"));
 // console.log(numberParser("0000jhgjghj"));
+
+const stringParser = input => {
+	if (input[0] !== `\"`) {
+		return null;
+	}
+
+	input = input.slice(1);
+
+	let a = input.indexOf(`\"`);
+	if (a === -1) {
+		return null;
+	}
+	const str = input.slice(0, input.indexOf(`\"`));
+	return [str, input.slice(str.length)];
+};
+
+console.log(stringParser('"hello\b"123'));
+console.log(stringParser('"abc\\"c"d"'));
+console.log(stringParser('""'));
+
+const commaParser = input => {
+	if (input.startsWith(",")) {
+		return [",", input.slice(1)];
+	}
+	return null;
+};
+
+const spaceParser = input => {
+	let regexSpace = /^\s+/;
+	let space = input.match(regexSpace);
+	if (space) {
+		return [space[0], input.slice(space[0].length)];
+	}
+	return null;
+};

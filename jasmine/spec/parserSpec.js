2134fd34d8aba69c105b1c4ba4123abfdee1419c
Archana Bansal <archanamittal0388@gmail.com>
2018-03-14 23:46:05 +0530

    Feat: Test cases added

---
+++ jasmine/spec/parserSpec.js
@@ -0,0 +1,118 @@
+/* global describe, it, expect, stringParser,arrayParser, numberParser, objectParser, JSONParser */
+
+// stringParser test cases---
+
+describe('String Parser', function () {
+  it('should return valid string if found', function () {
+    const obj = {
+      '"abc\\"c"d"': ['abc\\"c', 'd"'],
+      '"hello\n"123"': ['hello\n', '123"'],
+      '""': ['', '']
+    }
+    for (const key in obj) {
+      expect(stringParser(key)).toEqual(obj[key])
+    }
+  })
+
+  it('should return null if invalid string', function () {
+    const invalidString = [
+      '"dfgvbh', '"', '5767', '\\'
+    ]
+    invalidString.forEach((value) => {
+      expect(stringParser(value)).toEqual(null)
+    })
+  })
+})
+
+// arrayParser test cases---
+
+describe('Array Parser', function () {
+  it('should return valid array if found', function () {
+    const obj = {
+      '[ "a" , "b"]]': [ [ 'a', 'b' ], ']' ],
+      '["a\nb" , ["b", ["b", "c"]]]': [['a\nb', ['b', ['b', 'c']]], ''],
+      '["a" , 123]': [['a', 123], ''],
+      '[]': [[], '']
+    }
+    for (const key in obj) {
+      expect(arrayParser(key)).toEqual(obj[key])
+    }
+  })
+
+  it('should return null if invalid array', function () {
+    const invalidArray = [
+      '[ "a" ,]', '[ "a" 1 , "b"]', '[ "a" ,, "b"]', '["a" , null123,,hhhd]', '["a\\\\\n" , 1"23,   ,]', '["a" ,true,]'
+    ]
+    invalidArray.forEach((value) => {
+      expect(arrayParser(value)).toEqual(null)
+    })
+  })
+})
+
+// objectParser test cases---
+
+describe('Object Parser', function () {
+  it('should return valid object if found', function () {
+    const obj = {
+      '{"a":"abc"}': [ { a: 'abc' }, '' ],
+      '{"a":{"1":2},"b":123   ,  "c":[1,2]}': [ { a: { '1': 2 }, b: 123, c: [ 1, 2 ] }, '' ]
+    }
+    for (const key in obj) {
+      expect(objectParser(key)).toEqual(obj[key])
+    }
+  })
+
+  it('should return null if invalid object', function () {
+    const invalidObject = [
+      '{"1":123,2:"abc"}', '{,}', '{"a":"ab\\nc", , "b":}', '{"a":[1,1,2,]}', '{"a":{1:2}}', '{"a"', '{a}', ''
+    ]
+    invalidObject.forEach((value) => {
+      expect(objectParser(value)).toEqual(null)
+    })
+  })
+})
+
+// numberParser test cases---
+
+describe('Number Parser', function () {
+  it('should return valid number if found', function () {
+    const obj = {
+      '-123jhgjghj': [-123, 'jhgjghj']
+    }
+    for (const key in obj) {
+      expect(numberParser(key)).toEqual(obj[key])
+    }
+  })
+
+  it('should return null if invalid number', function () {
+    const invalidNumber = [
+      '"dfgvbh', '"', 'fg5767', '\\'
+    ]
+    invalidNumber.forEach((value) => {
+      expect(numberParser(value)).toEqual(null)
+    })
+  })
+})
+
+// JSONParser test cases--
+
+describe('JSON Parser', function () {
+  it('should return valid JSON if found', function () {
+    const obj = {
+      '[ "a" , "b"]': ['a', 'b'],
+      '{}': {}
+    }
+    for (const key in obj) {
+      expect(JSONParser(key)).toEqual(obj[key])
+    }
+  })
+
+  it('should return Invalid JSON if invalid JSON', function () {
+    const invalidJson = [
+      '[ "a" , "b"]]', '[a]', '"a" , "b"'
+    ]
+    invalidJson.forEach((value) => {
+      expect(JSONParser(value)).toEqual('Invalid JSON')
+    })
+  })
+})
--- parser.js
+++ parser.js
@@ -1,248 +1,186 @@
 const booleanParser = input => {
-	input = spaceParser(input)[1];
-	if (input.startsWith("true")) {
-		return [true, input.slice(4)];
-	}
+  input = spaceParser(input)[1]
+  if (input.startsWith('true')) {
+    return [true, input.slice(4)]
+  }
 
-	if (input.startsWith("false")) {
-		return [false, input.slice(5)];
-	}
+  if (input.startsWith('false')) {
+    return [false, input.slice(5)]
+  }
 
-	return null;
-};
+  return null
+}
 
 const nullParser = input => {
-	input = spaceParser(input)[1];
-	if (input.startsWith("null")) {
-		return [null, input.slice(4)];
-	}
-	return null;
-};
+  input = spaceParser(input)[1]
+  if (input.startsWith('null')) {
+    return [null, input.slice(4)]
+  }
+  return null
+}
 
 const numberParser = input => {
-	input = spaceParser(input)[1];
-	let regexNum = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/;
-	let num = input.match(regexNum);
-	if (num) {
-		return [parseFloat(num[0]), input.slice(num[0].length)];
-	}
-	return null;
-};
+  input = spaceParser(input)[1]
+  let regexNum = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/
+  let num = input.match(regexNum)
+  if (num) {
+    return [parseFloat(num[0]), input.slice(num[0].length)]
+  }
+  return null
+}
 
 const stringParser = input => {
-	input = spaceParser(input)[1];
-	if (input[0] !== '"') {
-		return null;
-	}
-	input = input.slice(1);
-	let str = "";
-	while (input[0] !== '"') {
-		if (input[0] === "\\") {
-			str += input.slice(0, 2);
-			input = input.slice(2);
-		}
-		str += input[0];
-		input = input.slice(1);
-	}
-	return [str, input.slice(1)];
-};
+  input = spaceParser(input)[1]
+  if (input[0] === '"') {
+    input = input.slice(1)
+    let str = ''
+    while (input && input[0] !== '"') {
+      if (input[0] === '\\') {
+        str += input.slice(0, 2)
+        input = input.slice(2)
+      }
+      str += input[0]
+      input = input.slice(1)
+    }
+    if (input[0] === '"') {
+      input = input.slice(1)
+      return [str, input]
+    }
+  }
+  return null
+}
 
 const spaceParser = input => {
-	let regexSpace = /^\s*/;
-	let space = input.match(regexSpace);
-	if (space) {
-		return [space[0], input.slice(space[0].length)];
-	}
-	return ["", input];
-};
+  let regexSpace = /^\s*/
+  let space = input.match(regexSpace)
+  if (space) {
+    return [space[0], input.slice(space[0].length)]
+  }
+  return ['', input]
+}
 
 const commaParser = input => {
-	input = spaceParser(input)[1];
-	if (input.startsWith(",")) {
-		return [",", input.slice(1)];
-	}
-	return null;
-};
+  input = spaceParser(input)[1]
+  if (input.startsWith(',')) {
+    return [',', input.slice(1)]
+  }
+  return null
+}
 
 const arrayParser = input => {
-	input = spaceParser(input)[1];
-	if (input[0] === "[") {
-		let arr = [];
-		input = input.slice(1);
-		let output;
-		while (input[0] !== "]") {
-			output = valueParser(input);
-			if (output) {
-				arr.push(output[0]);
-				input = output[1];
-			} else {
-				return "Invalid JSON";
-			}
-			if (commaParser(input)) {
-				input = commaParser(input)[1];
-				if (input[0] === "]") {
-					return "Invalid JSON";
-				}
-			} else if (input[0] !== "]" && commaParser(input) === null) {
-				return "Invalid JSON";
-			}
-		}
-		if (input[0] === "]") {
-			input = input.slice(1);
-			return [arr, input];
-		}
-	}
-	return null;
-};
+  input = spaceParser(input)[1]
+  if (input[0] === '[') {
+    let arr = []
+    input = input.slice(1)
+    let output
+    while (input[0] !== ']') {
+      output = valueParser(input)
+      if (output) {
+        arr.push(output[0])
+        input = output[1]
+      } else {
+        return null
+      }
+      if (commaParser(input)) {
+        input = commaParser(input)[1]
+        if (input[0] === ']') {
+          return null
+        }
+      } else if (input[0] !== ']' && commaParser(input) === null) {
+        return null
+      }
+    }
+    if (input[0] === ']') {
+      input = input.slice(1)
+      return [arr, input]
+    }
+  }
+  return null
+}
 
 const colonParser = input => {
-	input = spaceParser(input)[1];
-	if (input.startsWith(":")) {
-		return [":", input.slice(1)];
-	}
-	return null;
-};
+  input = spaceParser(input)[1]
+  if (input.startsWith(':')) {
+    return [':', input.slice(1)]
+  }
+  return null
+}
 
 const objectParser = input => {
-	input = spaceParser(input)[1];
+  input = spaceParser(input)[1]
 
-	if (input[0] === "{") {
-		let obj = {};
-		input = input.slice(1);
-		let key = [],
-			value = [];
+  if (input[0] === '{') {
+    input = input.slice(1)
+    let key = []
+    let value = []
+    let obj = {}
+    while (input[0] !== '}') {
+      if (stringParser(input)) {
+        key.push(stringParser(input)[0])
+        input = stringParser(input)[1]
+      } else {
+        return null
+      }
 
-		while (input[0] !== "}") {
-			if (stringParser(input)) {
-				key.push(stringParser(input)[0]);
-				input = stringParser(input)[1];
-			} else {
-				return "Invalid JSON";
-			}
-			if (colonParser(input)) {
-				input = colonParser(input)[1];
-				if (input[0] === "}") {
-					return "Invalid JSON";
-				}
-				if (valueParser(input)) {
-					value.push(valueParser(input)[0]);
-					input = valueParser(input)[1];
-				}
-			} else {
-				return "Invalid JSON";
-			}
-			if (commaParser(input)) {
-				input = commaParser(input)[1];
-				if (input[0] === "}") {
-					return "Invalid JSON";
-				}
-			} else if (input[0] !== "}" && commaParser(input) === null) {
-				return "Invalid JSON";
-			}
-		}
-		if (input[0] === "}") {
-			input = input.slice(1);
-		}
+      if (colonParser(input)) {
+        input = colonParser(input)[1]
+        if (input[0] === '}') {
+          return null
+        }
+        if (valueParser(input)) {
+          value.push(valueParser(input)[0])
+          input = valueParser(input)[1]
+        }
+      } else {
+        return null
+      }
+      if (commaParser(input)) {
+        input = commaParser(input)[1]
+        if (input[0] === '}') {
+          return null
+        }
+      } else if (input[0] !== '}' && commaParser(input) === null) {
+        return null
+      }
+    }
+    if (input[0] === '}') {
+      input = input.slice(1)
+    }
 
-		for (let i = 0; i < key.length; i++) {
-			obj[key[i]] = value[i];
-		}
-		return [obj, input];
-	}
-	return null;
-};
-
-const JSONParser = function(input) {
-	let result = valueParser(input);
-	if (result && result[1] !== "") {
-		console.log("invalid JSON");
-	} else if (result) {
-		console.log(JSON.stringify(result[0]));
-	} else {
-		console.log("invalid JSON");
-	}
-};
+    for (let i = 0; i < key.length; i++) {
+      obj[key[i]] = value[i]
+    }
+    return [obj, input]
+  }
+  return null
+}
 
 const factoryParser = (...parsers) => {
-	return function(input) {
-		for (let value of parsers) {
-			if (value(input)) {
-				return value(input);
-			}
-		}
-		return null;
-	};
-};
+  return function (input) {
+    for (let value of parsers) {
+      if (value(input)) {
+        return value(input)
+      }
+    }
+    return null
+  }
+}
 const valueParser = factoryParser(
-	nullParser,
-	booleanParser,
-	numberParser,
-	stringParser,
-	arrayParser,
-	objectParser
-);
+  nullParser,
+  booleanParser,
+  numberParser,
+  stringParser,
+  arrayParser,
+  objectParser
+)
 
-/*
------------Test cases-------------
-
-//boolean parser--
-
-console.log(booleanParser("true123"));
-console.log(booleanParser("123false"));
-console.log(booleanParser("fal123"));
-
-// null parser---
-
-console.log(nullParser("null123"));
-console.log(nullParser("123null"));
-console.log(nullParser("null"));
-console.log(nullParser("123"));
-
-//number parser--
-
-console.log(numberParser("12.38888jhgjghj"));
-console.log(numberParser("-123jhgjghj"));
-console.log(numberParser("7.71234e+2jhgjghj"));
-console.log(numberParser("0000jhgjghj"));
-
-//string parser---
-
-console.log(stringParser('"hello\n"123"'));
-console.log(stringParser('"abc\\"c"d"'));
-console.log(stringParser('""'));
-
-//array parser---
-
-console.log(arrayParser('[ "a" ,]'));
-console.log(arrayParser('[ "a" ,, "b"]'));
-console.log(arrayParser('[ "a" 1 , "b"]'));
-console.log(arrayParser('[ "a" , "b"]]'));
-console.log(arrayParser('["a\nb" , ["b", ["b", "c"]]]'));
-console.log(arrayParser('["a" , 123]'));
-console.log(arrayParser('["a" , null123,,hhhd]'));
-console.log(arrayParser('["a\\\\\n" , 1"23,   ,]'));
-console.log(arrayParser('["a" ,true,]'));
-console.log(arrayParser("[]"));
-
-
-// object parser---
-
-console.log(objectParser('{"1":123,2:"abc"}'));
-console.log(objectParser("{,}"));
-console.log(objectParser('{"a":"abc"}'));
-console.log(objectParser('{"a":"ab\\nc", , "b":}'));
-console.log(objectParser('{"a":[1,1,2,]}'));
-console.log(objectParser('{"a":{1:2}}'));
-console.log(objectParser('{"a":{"1":2},"b":123   ,  "c":[1,2]}'));
-console.log(objectParser('{"a"'));
-console.log(objectParser("{a}"));
-
-//JSON parser--
-
-JSONParser('[ "a" , "b"]]');
-JSONParser('[ "a" , "b"]');
-JSONParser("[a]");
-JSONParser("{}");
-JSONParser('"a" , "b"');
-
-*/
+const JSONParser = function (input) { // eslint-disable-line no-unused-vars
+  let result = valueParser(input)
+  if (result && result[1] !== '') {
+    return 'Invalid JSON'
+  } else if (result) {
+    return result[0]
+  } else {
+    return 'Invalid JSON'
+  }
+}
